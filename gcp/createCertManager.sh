ab1f2e6a870d2a5eadab8b7519242b089b62f87b
Mumtaz Khan <makcalif@yahoo.com>
2019-09-09 00:03:57 -0500

    cert manager create script

---
+++ gcp/createCertManager.sh
@@ -0,0 +1,13 @@
+
+kubectl delete crd certificates.certmanager.k8s.io 
+kubectl delete crd clusterissuers.certmanager.k8s.io
+kubectl delete crd issuers.certmanager.k8s.io 
+
+#helm search -l stable/cert-manager  (found out 0.2.1 is laterst)
+helm install --name cert-manager \
+    --namespace default \
+    --set ingressShim.defaultIssuerName=letsencrypt-prod \
+    --set ingressShim.defaultIssuerKind=ClusterIssuer \
+    stable/cert-manager \
+    --version v0.5.2
+
